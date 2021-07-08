ef34757320b88915a28b9d3359dbaf186b98a3b5
AnjuMaharjan <anju.mhrzn@gmail.com>
2021-07-08 21:42:51 +1000

    ngrok testing

--- JenkinsTest/Program.cs
+++ JenkinsTest/Program.cs
@@ -11,7 +11,7 @@ namespace JenkinsTest
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
-			Console.WriteLine("Hello again");
+			Console.WriteLine("Hello again-ngrok");
 			Console.WriteLine("Jenkins trigger added");
             Console.ReadLine();
         }
