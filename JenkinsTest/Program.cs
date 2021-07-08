8b98e7183bfbc2c29bcd5efa062cb77bf8adbc60
AnjuMaharjan <anju.mhrzn@gmail.com>
2021-07-08 19:59:13 +1000

    Line added in Program.cs

--- JenkinsTest/Program.cs
+++ JenkinsTest/Program.cs
@@ -10,6 +10,8 @@ namespace JenkinsTest
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
         }
     }
 }
